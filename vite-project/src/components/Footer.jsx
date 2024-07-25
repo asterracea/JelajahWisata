e3ba4bf9c8dc17a216a4180ac5feb84b5d57fc41
asterracea <alifwidya25@gmail.com>
2024-07-25 22:36:12 +0700

    fixing footer

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -8,25 +8,17 @@ function Footer() {
         <div className="container p-6">
             <div className="grid gap-4 lg:grid-cols-2">
             <div className="mb-6 md:mb-0">
-                <h5 className="mb-2 font-medium uppercase">Footer text</h5>
+            <h5 className="mb-2 font-medium  text-yellow-300 uppercase">Informasi</h5>
 
-                <p className="mb-4">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
-                atque ea quis molestias. Fugiat pariatur maxime quis culpa
-                corporis vitae repudiandae aliquam voluptatem veniam, est atque
-                cumque eum delectus sint!
+                <p className="mb-4 text-gray-900">
+                Jelajah Wisata adalah platform yang menyediakan informasi terkini dan terpercaya mengenai tempat-tempat wisata di Sekitar Kota Magetan. Temukan keindahan alam, sejarah, dan budaya nusantara.
                 </p>
             </div>
 
             <div className="mb-6 md:mb-0">
-                <h5 className="mb-2 font-medium uppercase">Footer text</h5>
-
-                <p className="mb-4">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
-                atque ea quis molestias. Fugiat pariatur maxime quis culpa
-                corporis vitae repudiandae aliquam voluptatem veniam, est atque
-                cumque eum delectus sint!
-                </p>
+                <h5 className="mb-2 font-medium  text-yellow-300 uppercase">Kontak kita</h5>
+                <p className=' text-gray-900'>jelajahwisata.com</p>
+                <p className='mb-4 text-gray-900'>+62 821-1826-5757</p>
             </div>
             </div>
         </div>
